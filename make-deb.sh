6fb8690adaffd73cc11eaf89a66c0babe3f5c9dc
sametatabasch <sametatabasch@gmail.com>
2025-05-26 00:08:04 +0300

    unzip bağımlılığı eklendi

--- make-deb.sh
+++ make-deb.sh
@@ -22,7 +22,7 @@ Version: $VERSION
 Section: admin
 Priority: optional
 Architecture: $ARCH
-Depends: bash, curl, apache2, certbot, jq
+Depends: bash, curl, apache2, certbot, jq, unzip
 Maintainer: $MAINTAINER
 Description: Apache tabanlı web siteleri yönetmek için terminal aracı
  Basit bir arayüz ile Apache VirtualHost oluşturur, WordPress kurar, geçici subdomain atar.
