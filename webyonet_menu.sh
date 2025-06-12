d3d967d0c30fe1167a550b332f0b78a1e1f5fa9b
sametatabasch <sametatabasch@gmail.com>
2025-06-12 13:17:31 +0300

    fix: correct typo in last_steps script call in create_site function

--- webyonet_menu.sh
+++ webyonet_menu.sh
@@ -41,7 +41,7 @@ create_site() {
         echo "❌ Desteklenmeyen web sunucusu: $WEB_SERVER"
         exit 1
     fi
-    ba sh $APPDIR/last_steps.sh
+    bash $APPDIR/last_steps.sh
 }
 
 change_domain() {
