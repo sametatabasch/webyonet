c50133194364d70ac9604b9a63ef18feb51e152b
sametatabasch <sametatabasch@gmail.com>
2025-07-01 01:26:54 +0300

    refactor: simplify backup script by removing unused functions and using rclone for uploads

--- dbBackupToYandex.sh
+++ dbBackupToYandex.sh
@@ -25,103 +25,13 @@ LOG_FILE="$BASE_BACKUP_DIR/backup-db-$DATE_MONTH.log"
 # Yandex.Disk remote adı (rclone config'de verdiğiniz isim)
 YANDEX_REMOTE="yadisk"
 
-function logger()
-{
-    echo "["`date "+%Y-%m-%d %H:%M:%S"`"] $1" >> "$LOG_FILE"
-}
-
 function echoLogger()
 {
     echo "$1"
     echo "["`date "+%Y-%m-%d %H:%M:%S"`"] $1" >> "$LOG_FILE"
 }
 
-function parseJson()
-{
-    local output
-    regex="(\"$1\":[\"]?)([^\",\}]+)([\"]?)"
-    [[ $2 =~ $regex ]] && output=${BASH_REMATCH[2]}
-    echo $output
-}
-
-function checkError()
-{
-    echo $(parseJson 'error' "$1")
-}
-
-# 'OK' - folder created or already exists on YD
-function createDir()
-{
-    local json_out
-    local json_error
-    json_out=`curl -s -X PUT -H "Authorization: OAuth $TOKEN" https://cloud-api.yandex.net/v1/disk/resources?path=/$DIR_NAME`
-    json_error=$(checkError "$json_out")
-    if [[ $json_error != '' ]]; then
-        if [[ $json_error == 'DiskPathPointsToExistentDirectoryError' ]]; then
-            echo 'OK'
-        else
-            logger "Directory '$DIR_NAME' not created. Error: $json_error"
-            echo ''
-        fi
-    else
-        echo 'OK'
-    fi
-}
-# $1 - file name in folder $DIR_NAME on YD
-function getUploadUrl()
-{
-    local json_out
-    local json_error
-    local output
-    json_out=`curl -s --header "Authorization: OAuth $TOKEN" https://cloud-api.yandex.net/v1/disk/resources/upload?path=/$DIR_NAME/$1`
-    json_error=$(checkError "$json_out")
-    if [[ $json_error != '' ]]; then
-        logger "URL for '$1' not created. Error: $json_error"
-        echo ''
-    else
-        output=$(parseJson 'href' "$json_out")
-        echo $output
-    fi
-}
-
-# $1 - path to folder with file
-# $2 - local file name for upload
-function uploadFile()
-{
-    local json_out
-    local json_error
-    local upload_url
-    upload_url=$(getUploadUrl "$2")
-    if [[ $upload_url != '' ]]
-    then
-        json_out=`curl -s -F "file=@$1/$2" --header "Authorization: OAuth $TOKEN" $upload_url`
-        json_error=$(checkError "$json_out")
-        if [[ $json_error != '' ]]
-        then
-            echoLogger "File '$2' not uploaded. Error: $json_error"
-        else
-            echoLogger "File '$2' uploaded to Yandex.Disk"
-        fi
-    else
-        echoLogger "Can not get upload URL. File '$2' not uploaded."
-    fi
-}
-
-mkdir -p "$BACKUP_DIR"
-if [[ ! -d "$BACKUP_DIR" ]]
-then
-    echo 'Can not create backup folder'
-    exit 1
-fi
-
 echoLogger 'Starting mysql backup'
-
-dirOK=$(createDir)
-if [[ $dirOK != 'OK' ]]
-then
-    echoLogger 'Error occured while creating folder on Yandex.Disk'
-    echoLogger 'Continue without uploading'
-fi
 
 databases=`mysql -u backup -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema|mysql|sys)"`
 
@@ -130,9 +40,13 @@ do
     echoLogger "Creating '$db' dump"
     dump_name="$db.sql.gz"
     mysqldump -u backup --force --opt --databases $db | gzip > "$BACKUP_DIR/$dump_name"
-    if [[ $dirOK == 'OK' ]]; then
-        echoLogger "Uploading '$dump_name' to Yandex.Disk"
-        uploadFile "$BACKUP_DIR" "$dump_name"
+    rclone copy "$BACKUP_DIR/$dump_name" "$YANDEX_REMOTE:$DIR_NAME/" --progress --transfers=4
+    if [[ $? -eq 0 ]]; then
+        echoLogger "File '$dump_name' uploaded to Yandex.Disk (rclone)"
+        rm -f "$BACKUP_DIR/$dump_name"
+        echoLogger "$dump_name silindi."
+    else
+        echoLogger "File '$dump_name' not uploaded. Error with rclone."
     fi
 done
 
@@ -141,3 +55,4 @@ echoLogger "Removing backup dir $BACKUP_DIR"
 rm -rf $BACKUP_DIR
 
 echoLogger 'All done!'
+echoLogger '--------------------------'
